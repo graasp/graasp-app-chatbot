fca5205711a87d18f209c412515a73553c254d19
LinaYahya <linaebe0@gmail.com>
2024-05-17 14:28:30 +0200

    fix: generate action id randomly

--- cypress/fixtures/mockActions.ts
+++ cypress/fixtures/mockActions.ts
@@ -7,13 +7,13 @@ const baseAction = {
   item: APP_ITEM,
   type: AppActionsType.Reply,
   member: MEMBERS.ANNA,
-  id: 'random',
   createdAt: '2020-02-07',
 };
 
 export const actions = [
   {
     ...baseAction,
+    id: (Math.random() + 1).toString(36).substring(2),
     data: {
       content:
         'Hello, Chatbot! Did you know that elephants are among the most intelligent and socially complex animals on Earth?    ',
@@ -21,6 +21,7 @@ export const actions = [
   },
   {
     ...baseAction,
+    id: (Math.random() + 1).toString(36).substring(2),
     data: {
       content:
         ' Exactly. They have highly developed brains, with a complex network of neurons that enable advanced social behaviors and learning capabilities.',
@@ -28,6 +29,8 @@ export const actions = [
   },
   {
     ...baseAction,
+    id: (Math.random() + 1).toString(36).substring(2),
+
     data: {
       content:
         'Absolutely. Their strong social structures play a crucial role in their survival and well-being, as they rely on cooperation for tasks like raising calves and finding food.',
@@ -35,6 +38,7 @@ export const actions = [
   },
   {
     ...baseAction,
+    id: (Math.random() + 1).toString(36).substring(2),
     data: {
       content:
         'Thats right. Their long-term memory is essential for navigating vast landscapes and passing down knowledge from one generation to the next.',
