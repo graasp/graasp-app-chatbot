843a005dff59f5f5efc1dbc1e7db983a42a546fc
LinaYahya <linaebe0@gmail.com>
2024-05-17 14:51:47 +0200

    feat: refactor mock actions by create a func to generate an action

--- cypress/fixtures/mockActions.ts
+++ cypress/fixtures/mockActions.ts
@@ -1,44 +1,49 @@
+import { AppItemType, Member } from '@graasp/sdk';
+import { ItemFactoryOutputType } from '@graasp/sdk/types/item/factory';
+
 import { AppActionsType } from '@/config/appActions';
 
 import { MEMBERS } from './members';
 import { APP_ITEM } from './mockItem';
 
-const baseAction = {
+const generateAction = (): {
+  item: ItemFactoryOutputType<AppItemType>;
+  type: 'reply_comment';
+  member: Member;
+  createdAt: string;
+  id: string;
+} => ({
   item: APP_ITEM,
   type: AppActionsType.Reply,
   member: MEMBERS.ANNA,
   createdAt: '2020-02-07',
-};
+  id: (Math.random() + 1).toString(36).substring(2),
+});
 
 export const actions = [
   {
-    ...baseAction,
-    id: (Math.random() + 1).toString(36).substring(2),
+    ...generateAction(),
     data: {
       content:
         'Hello, Chatbot! Did you know that elephants are among the most intelligent and socially complex animals on Earth?    ',
     },
   },
   {
-    ...baseAction,
-    id: (Math.random() + 1).toString(36).substring(2),
+    ...generateAction(),
     data: {
       content:
         ' Exactly. They have highly developed brains, with a complex network of neurons that enable advanced social behaviors and learning capabilities.',
     },
   },
   {
-    ...baseAction,
-    id: (Math.random() + 1).toString(36).substring(2),
-
+    ...generateAction(),
     data: {
       content:
         'Absolutely. Their strong social structures play a crucial role in their survival and well-being, as they rely on cooperation for tasks like raising calves and finding food.',
     },
   },
   {
-    ...baseAction,
-    id: (Math.random() + 1).toString(36).substring(2),
+    ...generateAction(),
     data: {
       content:
         'Thats right. Their long-term memory is essential for navigating vast landscapes and passing down knowledge from one generation to the next.',
