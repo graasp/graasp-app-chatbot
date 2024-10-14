246873342dbf286468992ce91a3029c8475bed7a
spaenleh <spaenleh@gmail.com>
2024-10-14 15:07:05 +0200

    fix: upgrade everything

--- cypress/fixtures/members.ts
+++ cypress/fixtures/members.ts
@@ -1,24 +1,18 @@
-import { Member, MemberType } from '@graasp/sdk';
+import { AccountType, CompleteMember, MemberFactory } from '@graasp/sdk';
 
-export const MEMBERS: { [key: string]: Member } = {
-  ANNA: {
+export const MEMBERS: { [key: string]: CompleteMember } = {
+  ANNA: MemberFactory({
     id: '0f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'anna',
-    type: MemberType.Individual,
+    type: AccountType.Individual,
     email: 'anna@graasp.org',
-    extra: {},
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  BOB: {
+  }),
+  BOB: MemberFactory({
     id: '1f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'bob',
-    type: MemberType.Individual,
+    type: AccountType.Individual,
     email: 'bob@graasp.org',
-    extra: {},
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
+  }),
 };
 
 export const CURRENT_MEMBER = MEMBERS.ANNA;
