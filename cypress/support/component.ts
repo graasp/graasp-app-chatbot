c34c23cf3926c395669f15e8c4437c0707d57642
spaenleh <spaenleh@gmail.com>
2025-06-16 10:26:18 +0200

    fix: cypress upgrade and import path fix

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -15,7 +15,7 @@
 // Import commands.js using ES2015 syntax:
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import { mount } from 'cypress/react18';
+import { mount } from 'cypress/react';
 
 import './commands';
 
